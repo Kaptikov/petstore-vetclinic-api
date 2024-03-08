ff20a2c61bba482a3bd40ab68fc4b8d4da08464e
Kaptikov <igor.zabirow02@mail.ru>
2024-03-08 12:36:59 +0500

    Add project files.

---
+++ petstore-vetclinic-api/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace petstore_vetclinic_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    }
+}
